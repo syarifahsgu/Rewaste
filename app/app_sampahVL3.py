8c4f018ba697e3abc05aa35ca1fba74ebe615edb
Syarifah Agustiani <90826928+syarifahsgu@users.noreply.github.com>
2025-09-22 20:43:34 +0700

    Update app_sampahVL3.py

--- app/app_sampahVL3.py
+++ app/app_sampahVL3.py
@@ -12,7 +12,7 @@ from huggingface_hub import hf_hub_download
 
 HF_TOKEN = os.getenv("HF_TOKEN")  # pastikan sudah set di terminal atau Streamlit Secrets
 MODEL_REPO = "syarifahsgu/rewaste_model_efficientnet"
-MODEL_FILENAME = "best_model_efficientnet.keras"
+MODEL_FILENAME = "best_mobilenetv2_data_split.h5"
 
 # -------------------------------
 # Load model dari HF
